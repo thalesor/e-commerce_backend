55ba7b12f6769856bbbec8f8b71d232a9cfdde2c
Thales Ruano <thalesoruano@gmail.com>
2022-02-10 11:45:32 -0300

    feat cadastro books

---
+++ controllers/categoryController.js
@@ -0,0 +1,74 @@
+import { dbService } from '../services/db-service.js';
+
+async function getCategories(req, res)
+{
+    try
+    {
+        const categories = await dbService.find("categories",
+        {}
+        );
+        if(categories.length)
+        {
+            if(!res)
+            return categories;
+            else
+            {
+                res.status(200).json(categories);
+                return;
+            }
+        }
+        const response = `
+        Não há categorias cadastradas!`;
+        console.log(response);
+        if(res)
+        res.status(500).send(response);
+    }
+    catch(err)
+    {
+        const response = `
+        Erro no servidor ao tentar retornar as categorias cadastradas!`;
+        console.log(response);
+        if(res)
+        res.status(500).send(response);
+    } 
+}
+
+async function registrarCategorias()
+{
+    const categories = await getCategories();
+    if(!categories?.length)
+    {
+        try
+        {
+            const result = await dbService.insertMany("categories",
+            [
+                { name: "Terror"},
+                {  name: "Mistério"},
+                { name: "Culinária" }
+            ]
+            );
+            if(result)
+            {
+                console.log("Categorias cadastradas com sucesso na base de dados");
+                return;
+            }
+                console.log("Erro no banco de dados ao tentar cadastrar categorias");
+            return;
+        }
+        catch(err)
+        {
+            console.log("Erro no servidor ao tentar cadastrar as categorias");
+            return;
+        } 
+    }
+    else
+    {
+        console.log("Não será necessário cadastrar categorias");
+        return
+    }
+}
+
+export { 
+    getCategories,
+    registrarCategorias
+}
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import { postBook } from './controllers/bookController.js';
+import { getCategories, registrarCategorias} from './controllers/categoryController.js';
 import { validateBook } from './services/joi-service.js';
 import { validation } from './validationMiddleware.js';
 
 const app = express();
-dayjs.locale('pt-br');
 app.use(json());
 app.options('*', cors());
 app.use(cors());
 
 app.post('/book', validation(validateBook), postBook);
+app.get('/categories', getCategories);
+
+registrarCategorias();
 
 app.listen('5000', (port) => {
 	console.log(`Server running :^)`);
